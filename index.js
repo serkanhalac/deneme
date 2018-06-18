f7bb8603a4a1db2dc1f010d8a4faca91717d6582
serkanhalac <39057693+serkanhalac@users.noreply.github.com>
2018-06-18 13:55:48 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var http = require("http");
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-http.createServer(function (request, response) {
-response.writeHead(200, {'Content-Type': 'text/plain'});
-response.end('Hello World\n');
-}).listen(8081);
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
